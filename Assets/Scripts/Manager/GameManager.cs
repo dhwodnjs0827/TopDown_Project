dc24c1649508bbee4ca1da1b780f0f93a6471d98
Jaewon <dhwodnjs0827@naver.com>
2025-02-17 20:55:33 +0900

    UI 구성과 인터페이스 디자인

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -6,6 +6,9 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
+    private UIManager uiManager;
+    public static bool isFirstLoading = true;
+
     public PlayerController player
     {
         get; private set;
@@ -22,18 +25,38 @@ public class GameManager : MonoBehaviour
         player = FindObjectOfType<PlayerController>();
         player.Init(this);
 
+        uiManager = FindObjectOfType<UIManager>();
+
+        _playerResourceController = player.GetComponent<ResourceController>();
+        _playerResourceController.RemoveHealthChangeEvent(uiManager.ChangePlayerHP);
+        _playerResourceController.AddHealthChangeEvent(uiManager.ChangePlayerHP);
+
         enemyManager = GetComponentInChildren<EnemyManager>();
         enemyManager.Init(this);
     }
 
+    private void Start()
+    {
+        if (!isFirstLoading)
+        {
+            StartGame();
+        }
+        else
+        {
+            isFirstLoading = false;
+        }
+    }
+
     public void StartGame()
     {
+        uiManager.SetPlayGame();
         StartNextWave();
     }
 
     void StartNextWave()
     {
         currentWaveIndex += 1;
+        uiManager.ChangeWave(currentWaveIndex);
         enemyManager.StartWave(1 + currentWaveIndex / 5);
     }
 
@@ -45,13 +68,6 @@ public class GameManager : MonoBehaviour
     public void GameOver()
     {
         enemyManager.StopWave();
-    }
-
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            StartGame();
-        }
+        uiManager.SetGameOver();
     }
 }
